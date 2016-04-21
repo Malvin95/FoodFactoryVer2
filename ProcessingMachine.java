a9577d69a54b7e3b591baaeb95418835020c3771
Malvin95 <Malvinharding@hotmail.co.uk>
2016-04-21 20:22:55 +0100

    Update ProcessingMachine.java

--- ProcessingMachine.java
+++ ProcessingMachine.java
@@ -1,25 +1,41 @@
-import java.util.Random
+import java.util.ArrayList;
+import java.util.Random;
 /**
 *The ProcessingMachine class : class responsible for processing food.
 *author : Nelson Poon
-*/
+*
 
 * @param: probablility of a jam happening (double) inherited from super class
 * @param : rndm : generates a random item out of queue to begin processing
 */
-public class ProcessingMachine {
+
+public class ProcessingMachine extends Machine {
 	static final int Process_Time1 = 20;
 	static final int Process_Time2 = 22;
-	public Food food;
+	protected ArrayList<Product> PerishQ1; //queue waiting for food to be processed
+	protected ArrayList<Product> PerishQ2; //queue waiting for food to be processed
+	protected ArrayList<Product> NPerishQ; //queue waiting for food to be processed
+	public Product product;
 	
 	
 
-public ProcessingMachine(double prob,Random rndm){
-	super(prob, rndm);
-}
+	public ProcessingMachine(double probability,Random rndm)
+	{
+		super(probability, rndm);//
+		PerishQ1 = new ArrayList<Product>();//Holds Perishable Product Types
+		PerishQ2 = new ArrayList<Product>();//Holds Perishable Product Types
+		NPerishQ = new ArrayList<Product>();//Holds NonPerishable Product Types
+	}
+
+	public ProcessingMachine()
+	{
+		
+	}
+
+
 
 public void processing(){ //main processing method
-	this.jam(); //check if machine jam
+	Simulator.jam(); //check if machine jam
 	if (this.type == null && !this.foodQueue.isEmpty(){
 		this.type = this.foodqueue(); //takes food in queue into the machine
 		}
